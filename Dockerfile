14494ef50f5dc65c07173fcc9f366f1ab474eb75
luis mayta <slovacus@gmail.com>
2021-08-11 21:53:04 -0500

    fix: implement load go path (#21)

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,8 @@ LABEL maintainer="Team Hadenlabs <hello@hadenlabs.com>" \
       org.label-schema.version=$VERSION \
       org.label-schema.schema-version="1.0"
 
+WORKDIR /github/workspace
+
 ENV PATH $PATH:/go/bin:/usr/local/go/bin:/root/.local/bin:/usr/bin:/usr/local/bin
 
 COPY provision/script/entrypoint.sh /entrypoint.sh
--- provision/script/entrypoint.sh
+++ provision/script/entrypoint.sh
@@ -4,6 +4,11 @@ if [ -z "${GITHUB_WORKSPACE}" ]; then
     export GITHUB_WORKSPACE="${PWD}"
 fi
 
+# The following environment variables will be provided by the environment automatically: GITHUB_WORKSPACE
+mkdir -p "${GITHUB_WORKSPACE}"
+
+cd "${GITHUB_WORKSPACE}" || return
+
 echo "==> Running pre-commit"
 
 pre-commit install
